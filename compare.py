4ebca3da37739f91f6cb8961c908ad35dc174780
James Douglass <jdouglass@stanford.edu>
2021-10-22 10:56:43 -0700

    Taking the maxiimum pixel value of other, restoration, writing out new rasters.

--- compare.py
+++ compare.py
@@ -16,6 +16,7 @@ LOGGER = logging.getLogger(__name__)
 BASELINE = 'restoration'
 PREFIX = 'noxn_in_drinking_water'
 BYTE_NODATA = 255
+FLOAT32_NODATA = float(numpy.finfo(numpy.float32).min)
 
 
 def main():
@@ -27,14 +28,14 @@ def main():
     tg_directory = os.path.join(target_workspace, '_taskgraph_db')
 
     graph = taskgraph.TaskGraph(tg_directory, n_workers=16)
-    #graph = taskgraph.TaskGraph(tg_directory, n_workers=-1)
+    # graph = taskgraph.TaskGraph(tg_directory, n_workers=-1)
 
     restoration_scenario_raster = os.path.join(
         source_directory, f'{PREFIX}_{BASELINE}.tif')
     source_raster_info = pygeoprocessing.get_raster_info(
         restoration_scenario_raster)
 
-    target_raster_paths = []
+    qa_raster_paths = []
     for scenario_raster in glob.glob(os.path.join(source_directory, '*.tif')):
         LOGGER.info(f'Making a task for {scenario_raster}')
         if f'{PREFIX}_{BASELINE}.tif' in scenario_raster:
@@ -45,9 +46,9 @@ def main():
         # No need to assert raster dimensions ahead of time, just add to graph.
         # raster_calculator will assert dimensions.
         scenario_raster_info = pygeoprocessing.get_raster_info(scenario_raster)
-        target_raster_path = os.path.join(
+        qa_raster_path = os.path.join(
             target_workspace, f'qa_{PREFIX}_{scenario}.tif')
-        target_raster_paths.append(target_raster_path)
+        qa_raster_paths.append(qa_raster_path)
         graph.add_task(
             pygeoprocessing.raster_calculator,
             args=(
@@ -56,22 +57,39 @@ def main():
                  (source_raster_info['nodata'][0], 'raw'),
                  (scenario_raster_info['nodata'][0], 'raw')],
                 _check_values,
-                target_raster_path,
+                qa_raster_path,
                 gdal.GDT_Byte,
                 BYTE_NODATA),
-            target_path_list=[target_raster_path],
+            target_path_list=[qa_raster_path],
             task_name=f'check {scenario}')
+
+        max_raster_path = os.path.join(
+            target_workspace, f'max_qa_restoration_{PREFIX}_{scenario}.tif')
+        graph.add_task(
+            pygeoprocessing.raster_calculator,
+            args=(
+                [(restoration_scenario_raster, 1),
+                 (scenario_raster, 1),
+                 (qa_raster_path, 1),
+                 (source_raster_info['nodata'][0], 'raw'),
+                 (scenario_raster_info['nodata'][0], 'raw')],
+                _take_max,
+                max_raster_path,
+                gdal.GDT_Float32,
+                FLOAT32_NODATA),
+            target_path_list=[max_raster_path],
+            task_name=f'max {scenario}')
 
     graph.close()
     graph.join()
 
     summary_file = os.path.join(target_workspace, 'summary.txt')
     with open(summary_file, 'w') as summary:
-        for target_raster_path in target_raster_paths:
-            raster = gdal.Open(target_raster_path)
+        for qa_raster_path in qa_raster_paths:
+            raster = gdal.Open(qa_raster_path)
             band = raster.GetRasterBand(1)
             min, max, mean, stddev = band.GetStatistics(0, 0)
-            summary_string = f'{target_raster_path}, min:{min}, max:{max}, mean:{mean}, stddev:{stddev}\n'
+            summary_string = f'{qa_raster_path}, min:{min}, max:{max}, mean:{mean}, stddev:{stddev}\n'
             summary.write(summary_string)
 
     for line in open(summary_file):
@@ -97,6 +115,20 @@ def _check_values(restoration, other, restoration_nodata, other_nodata):
     target_matrix[valid] = (restoration[valid] > other[valid])
 
     return target_matrix
+
+
+def _take_max(restoration, other, qa, restoration_nodata, other_nodata):
+    target_max_matrix = numpy.full(
+        other.shape, FLOAT32_NODATA, dtype=numpy.float32)
+
+    valid = (qa != BYTE_NODATA)
+
+    if numpy.count_nonzero(valid) == 0:
+        return target_max_matrix
+
+    target_max_matrix[valid] = numpy.maximum(restoration[valid], other[valid])
+
+    return target_max_matrix
 
 
 def test():
